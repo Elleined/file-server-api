5e3edcc0bfcd72f6102bf695a4616185a1cd0208
Elleined <demadegu@gmail.com>
2024-05-24 22:20:27 +0800

    Added request package and validation

---
+++ src/main/java/com/elleined/image_server_api/request/ImageRequest.java
@@ -0,0 +1,30 @@
+package com.elleined.image_server_api.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+public class ImageRequest extends Request {
+    @NotBlank(message = "Please provide the uuid. This will be your key access the image.")
+    private String uuid;
+
+    private String description;
+    private String additionalInformation;
+
+    @Positive(message = "Please provide the image format")
+    private int imageFormatId;
+
+    @Positive(message = "Please provide the project id you want this image to be saved.")
+    private int projectId;
+
+    // image is in separate @RequestPart
+}
---
+++ src/main/java/com/elleined/image_server_api/request/Request.java
@@ -0,0 +1,13 @@
+package com.elleined.image_server_api.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+public class Request {
+}
