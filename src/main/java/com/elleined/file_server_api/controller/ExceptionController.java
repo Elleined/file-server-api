0f676f2f94a710ee328f56af02b8c13e00c54eca
Elleined <demadegu@gmail.com>
2025-06-25 14:39:09 +0800

    remove apiresponse and added contraintvalidation in controller advice

--- src/main/java/com/elleined/file_server_api/controller/ExceptionController.java
+++ src/main/java/com/elleined/file_server_api/controller/ExceptionController.java
@@ -1,8 +1,9 @@
 package com.elleined.file_server_api.controller;
 
-import com.elleined.file_server_api.dto.APIResponse;
+import java.lang.String;
 import com.elleined.file_server_api.exception.SystemException;
 import com.elleined.file_server_api.exception.resource.ResourceException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -16,32 +17,36 @@ import java.util.List;
 @ControllerAdvice
 public class ExceptionController {
 
-    @ExceptionHandler(ResourceException.class)
-    public ResponseEntity<APIResponse> handleResourceException(ResourceException ex) {
-        var responseMessage = new APIResponse(HttpStatus.NOT_FOUND, ex.getMessage());
-        return new ResponseEntity<>(responseMessage, HttpStatus.NOT_FOUND);
-    }
-
     @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public ResponseEntity<APIResponse> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex) {
-        var responseMessage = new APIResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
-        return new ResponseEntity<>(responseMessage, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler({
-            SystemException.class,
-    })
-    public ResponseEntity<APIResponse> handleSystemException(RuntimeException ex) {
-        var responseMessage = new APIResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
-        return new ResponseEntity<>(responseMessage, HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(ResourceException.class)
+    public ResponseEntity<String> handleResourceException(ResourceException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(SystemException.class)
+    public ResponseEntity<String> handleSystemException(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(BindException.class)
-    public ResponseEntity<List<APIResponse>> handleBindException(BindException ex) {
-        List<APIResponse> errors = ex.getBindingResult().getAllErrors().stream()
+    public ResponseEntity<List<String>> handleBindException(BindException ex) {
+        List<String> errors = ex.getBindingResult().getAllErrors().stream()
                 .map(ObjectError::getDefaultMessage)
-                .map(errorMessage -> new APIResponse(HttpStatus.BAD_REQUEST, errorMessage))
                 .toList();
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<String>> handleConstraintViolationException(ConstraintViolationException ex) {
+        List<String> errors = ex.getConstraintViolations().stream()
+                .map(violation -> String.format("%s: %s", violation.getPropertyPath().toString(), violation.getMessage()))
+                .toList();
+
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
