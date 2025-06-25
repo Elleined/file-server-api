29d2ba98008e2dc4b774be5efbccc1bc572b3d98
Elleined <demadegu@gmail.com>
2025-06-25 21:07:11 +0800

    added annotations in folderservice

--- src/main/java/com/elleined/file_server_api/folder/FolderServiceImpl.java
+++ src/main/java/com/elleined/file_server_api/folder/FolderServiceImpl.java
@@ -1,18 +1,29 @@
 package com.elleined.file_server_api.folder;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import java.nio.file.Path;
 
+@Slf4j
 @Service
+@Validated
+@RequiredArgsConstructor
 public class FolderServiceImpl implements FolderService {
 
     @Value("${UPLOAD_PATH}")
     private String uploadPath;
 
     @Override
-    public Path getUploadPath() {
-        return Path.of(uploadPath);
+    public Path save(String folder) {
+        return null;
+    }
+
+    @Override
+    public void delete(String folder) {
+
     }
 }
