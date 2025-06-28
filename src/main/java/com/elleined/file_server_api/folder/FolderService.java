51049ae879858513dc77650d31f3974ad229dc56
Elleined <demadegu@gmail.com>
2025-06-28 23:26:10 +0800

    add robust checking of folder creation

--- src/main/java/com/elleined/file_server_api/folder/FolderService.java
+++ src/main/java/com/elleined/file_server_api/folder/FolderService.java
@@ -1,12 +1,59 @@
 package com.elleined.file_server_api.folder;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public interface FolderService {
-    void create(@NotBlank String folder) throws IOException;
+    void create(@Size(max = 25) @NotBlank String folder) throws IOException;
     void remove(@NotBlank String folder) throws IOException;
-    Path getUploadPath();
+    Path get(@NotBlank String folder) throws IOException;
+    Path getUploadPath() throws IOException;
+
+    default boolean isNullOrBlank(String folder) {
+        return folder == null || folder.isBlank();
+    }
+
+    default boolean hasInvalidLength(String folder) {
+        return folder.length() > 25;
+    }
+
+    default boolean hasInvalidCharacters(String folder) {
+        return folder.contains(".") ||
+                folder.contains("/") ||
+                folder.contains("\\") ||
+                folder.contains("%");
+    }
+
+    default boolean isSymbolicLink(Path folder) {
+        return Files.isSymbolicLink(folder);
+    }
+
+    default Path normalize(String folder) throws IOException {
+        Path uploadPath = this.getUploadPath();
+        Path normalizePath = Paths.get(folder.strip())
+                .getFileName()
+                .normalize();
+
+        return uploadPath.resolve(normalizePath).normalize();
+    }
+
+    default boolean isInUploadPath(Path folderPath) throws IOException {
+        Path uploadPath = this.getUploadPath();
+        return folderPath.startsWith(uploadPath);
+    }
+
+    default boolean exists(Path folderPath) {
+        return Files.exists(folderPath, LinkOption.NOFOLLOW_LINKS);
+    }
+
+    default boolean isAllowed(Path folderPath) {
+        return Files.isWritable(folderPath) &&
+                Files.isReadable(folderPath);
+    }
 }
--- src/main/java/com/elleined/file_server_api/folder/FolderServiceImpl.java
+++ src/main/java/com/elleined/file_server_api/folder/FolderServiceImpl.java
@@ -4,12 +4,21 @@ import com.elleined.file_server_api.exception.FileServerAPIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+
+/*
+    normalize method: removes the unnecessary dot(.) and slash(/, \) of specified folder
+    toAbsolutePath method: get the absolute path of the specified path
+    getFileName method: only get the last entry of specified folder
+*/
 
 @Slf4j
 @Service
@@ -20,37 +29,61 @@ public class FolderServiceImpl implements FolderService {
     @Value("${UPLOAD_PATH}")
     private String uploadPath;
 
+    @Async
     @Override
     public void create(String folder) throws IOException {
         Path uploadPath = this.getUploadPath();
-        Path sanitizeFolder = Paths.get(folder)
-                .getFileName()
-                .normalize();
 
-        Path folderPath = uploadPath.resolve(sanitizeFolder).normalize();
-        if (!folderPath.startsWith(uploadPath))
+        // 1. Check for null or blank of specified folder
+        if (this.isNullOrBlank(folder))
+            throw new FileServerAPIException("Folder name cannot be null or blank");
+
+        // 2. Check if specified folder size with max of 25
+        if (this.hasInvalidLength(folder))
+            throw new FileServerAPIException("Folder name cannot be longer than 25 characters");
+
+        // 3. Check if specified folder contain invalid characters
+        if (this.hasInvalidCharacters(folder))
+            throw new FileServerAPIException("Folder name cannot contain invalid characters");
+
+        // 4. Normalize the folder
+        Path folderPath = this.normalize(folder);
+
+        // 5. Check if folderPath is symlink
+        if (this.isSymbolicLink(folderPath))
+            throw new FileServerAPIException("Symbolic links are not allowed");
+
+        // 6. Checks if the folderPath is within the upload directory
+        if (!this.isInUploadPath(folderPath))
             throw new FileServerAPIException("Attempted traversal attack");
 
-        if (Files.exists(folderPath, LinkOption.NOFOLLOW_LINKS))
-            throw new FileServerAPIException("Folder already exists");
+        // 7. Finally check if the folder name already exists
+        if (this.exists(folderPath))
+            throw new FileServerAPIException("Folder name already exists");
 
-        Files.createDirectories(folderPath);
+        Files.createDirectories(folderPath, PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rw-r--r--")));
         log.info("Folder created successfully {}", folderPath.toAbsolutePath());
     }
 
+    @Async
     @Override
     public void remove(String folder) throws IOException {
         Path uploadPath = this.getUploadPath();
-        Path sanitizeFolder = Paths.get(folder)
+        Path sanitizeFolder = Paths.get(folder.strip())
                 .getFileName()
                 .normalize();
-
+        System.out.println("Raw " + folder);
+        System.out.println("Sanitize " + sanitizeFolder);
         Path folderPath = uploadPath.resolve(sanitizeFolder).normalize();
+
         if (!folderPath.startsWith(uploadPath))
             throw new FileServerAPIException("Attempted traversal attack");
 
         if (folderPath.equals(uploadPath))
             throw new FileServerAPIException("Cannot delete root upload folder");
+
+        if (Files.isSymbolicLink(folderPath))
+            throw new FileServerAPIException("Symbolic links are not allowed");
 
         if (!Files.exists(folderPath, LinkOption.NOFOLLOW_LINKS))
             throw new FileServerAPIException("Folder does not exist");
@@ -73,9 +106,15 @@ public class FolderServiceImpl implements FolderService {
     }
 
     @Override
-    public Path getUploadPath() {
+    public Path get(String folder) throws IOException {
+        return null;
+    }
+
+    @Override
+    public Path getUploadPath() throws IOException {
         return Paths.get(uploadPath.strip())
                 .toAbsolutePath()
-                .normalize();
+                .normalize()
+                .toRealPath(LinkOption.NOFOLLOW_LINKS);
     }
 }
