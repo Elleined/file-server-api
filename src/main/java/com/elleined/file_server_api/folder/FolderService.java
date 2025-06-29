4112950e19f5a4edf6b3d1bb9f5b768a40c2e073
Elleined <demadegu@gmail.com>
2025-06-29 10:17:36 +0800

    remove @size in folder service

--- src/main/java/com/elleined/file_server_api/folder/FolderService.java
+++ src/main/java/com/elleined/file_server_api/folder/FolderService.java
@@ -1,13 +1,12 @@
 package com.elleined.file_server_api.folder;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 import java.io.IOException;
 import java.nio.file.Path;
 
 public interface FolderService {
-    void create(@Size(max = 25) @NotBlank String folder) throws IOException;
+    void create(@NotBlank String folder) throws IOException;
     void remove(@NotBlank String folder) throws IOException;
     Path get(@NotBlank String folder) throws IOException;
     Path getUploadPath() throws IOException;
