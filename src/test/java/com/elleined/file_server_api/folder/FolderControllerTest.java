f1517e5f849f68267d463787439b176cf4aee560
Elleined <demadegu@gmail.com>
2025-07-13 12:24:26 +0800

    add unit tests for FolderController: implement save method test with MockMvc and Mockito

---
+++ src/test/java/com/elleined/file_server_api/folder/FolderControllerTest.java
@@ -0,0 +1,54 @@
+package com.elleined.file_server_api.folder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FolderController.class)
+class FolderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private FolderService folderService;
+
+    @Test
+    void save_HappyPath() throws IOException {
+        // Pre defined values
+
+        // Expected Value
+        UUID folder = UUID.randomUUID();
+
+        // Mock data
+
+        // Set up method
+
+        // Stubbing methods
+        when(folderService.save()).thenReturn(folder);
+
+        // Calling the method
+        MvcResult mvcResult = assertDoesNotThrow(() -> mockMvc.perform(post("/folders"))
+                .andExpect(status().isOk())
+                .andReturn());
+
+        // Behavior Verifications
+        verify(folderService).save();
+
+        // Assertions
+        assertTrue(mvcResult.getResponse().getContentAsString().contains(folder.toString()));
+    }
+}
