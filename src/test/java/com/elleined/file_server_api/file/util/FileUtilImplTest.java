ae598644bc560df06cc4e1ddea427728dde017ed
Elleined <demadegu@gmail.com>
2025-07-22 13:16:50 +0800

    Test FileUtilImpl methods: getFileExtension for PDF, PNG, JPEG; checksum for file integrity; stream response creation

--- src/test/java/com/elleined/file_server_api/file/util/FileUtilImplTest.java
+++ src/test/java/com/elleined/file_server_api/file/util/FileUtilImplTest.java
@@ -2,28 +2,30 @@ package com.elleined.file_server_api.file.util;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class FileUtilImplTest {
 
     private final InputStream pdfFile = getClass().getClassLoader().getResourceAsStream("pdf.pdf");
     private final InputStream pngFile = getClass().getClassLoader().getResourceAsStream("png.png");
+
     @InjectMocks
     private FileUtilImpl fileUtil;
 
     @Test
-    void getFileExtension_HappyPath() {
+    void getFileExtension_PDF_HappyPath() {
         // Pre defined values
 
         // Expected Value
@@ -46,68 +48,118 @@ class FileUtilImplTest {
     }
 
     @Test
-    void checksum_HappyPath() throws IOException {
+    void getFileExtension_PNG_HappyPath() {
         // Pre defined values
 
         // Expected Value
+        String expectedExtension = "png";
 
         // Mock data
-        MultipartFile file = new MockMultipartFile("file", pdfFile);
+        MediaType mediaType = MediaType.IMAGE_PNG;
 
         // Set up method
 
         // Stubbing methods
+        String actual = assertDoesNotThrow(() -> fileUtil.getFileExtension(mediaType));
 
         // Calling the method
-//        String expected = assertDoesNotThrow(() -> fileUtil.checksum(file));
-//        String actual = assertDoesNotThrow(() -> fileUtil.checksum(file));
 
         // Behavior Verifications
 
         // Assertions
-//        assertEquals(expected, actual);
+        assertEquals(expectedExtension, actual);
     }
 
     @Test
-    void checksum_ShouldReturnFalse_IfFileHasBeenModified() throws IOException {
+    void getFileExtension_JPEG_HappyPath() {
         // Pre defined values
 
         // Expected Value
+        String expectedExtension = "jpg";
 
         // Mock data
-        MultipartFile pdfFile = new MockMultipartFile("file", this.pdfFile);
-        MultipartFile pngFile = new MockMultipartFile("file", this.pngFile);
+        MediaType mediaType = MediaType.IMAGE_JPEG;
 
         // Set up method
 
         // Stubbing methods
+        String actual = assertDoesNotThrow(() -> fileUtil.getFileExtension(mediaType));
 
         // Calling the method
-//        String expected = assertDoesNotThrow(() -> fileUtil.checksum(pdfFile));
-//        String actual = assertDoesNotThrow(() -> fileUtil.checksum(pngFile));
 
         // Behavior Verifications
 
         // Assertions
-//        assertNotEquals(expected, actual);
+        assertEquals(expectedExtension, actual);
     }
 
     @Test
-    void stream_HappyPath() {
+    void checksum_HappyPath(@TempDir Path tempDir) throws IOException {
         // Pre defined values
 
         // Expected Value
 
         // Mock data
+        Path file = Files.createTempFile(tempDir, "pdf", ".pdf");
+        String expected = "47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFU";
 
         // Set up method
 
         // Stubbing methods
 
         // Calling the method
+        String actual = assertDoesNotThrow(() -> fileUtil.checksum(file));
 
         // Behavior Verifications
 
         // Assertions
+        assertEquals(expected, actual, "actual and expected checksum should match");
+    }
+
+    @Test
+    void checksum_ShouldNotEqual_IfFileHasBeenModified(@TempDir Path tempDir) throws IOException {
+        // Pre defined values
+
+        // Expected Value
+
+        // Mock data
+        Path realFile = Files.createTempFile(tempDir, "pdf", ".pdf");
+        Path modifiedFile = Files.write(realFile, "Hello World".getBytes());
+
+        String realFileChecksum = "47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFU";
+
+        // Set up method
+
+        // Stubbing methods
+
+        // Calling the method
+        String actual = assertDoesNotThrow(() -> fileUtil.checksum(modifiedFile));
+
+        // Behavior Verifications
+
+        // Assertions
+        assertNotEquals(realFileChecksum, actual, "realFileChecksum should not match with actual because realFile has been modified");
+    }
+
+    @Test
+    void stream_HappyPath(@TempDir Path tempDir) throws IOException {
+        // Pre defined values
+
+        // Expected Value
+
+        // Mock data
+        Path file = Files.createTempFile(tempDir, "pdf", ".pdf");
+
+        // Set up method
+
+        // Stubbing methods
+
+        // Calling the method
+        StreamingResponseBody response = assertDoesNotThrow(() -> fileUtil.stream(file));
+
+        // Behavior Verifications
+
+        // Assertions
+        assertNotNull(response);
     }
 }
