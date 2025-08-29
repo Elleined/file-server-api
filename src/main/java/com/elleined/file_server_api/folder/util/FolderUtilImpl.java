ec42d4e16e435070e5f1c7c0af1d2aab90e0b38b
elleined <demadegu@gmail.com>
2025-08-29 15:15:11 +0800

    Added nullaway and solve its issues

--- src/main/java/com/elleined/file_server_api/folder/util/FolderUtilImpl.java
+++ src/main/java/com/elleined/file_server_api/folder/util/FolderUtilImpl.java
@@ -1,6 +1,5 @@
 package com.elleined.file_server_api.folder.util;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +9,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Service
-@RequiredArgsConstructor
 public class FolderUtilImpl implements FolderUtil {
 
-    @Value("${UPLOAD_PATH}")
-    private String uploadPath;
+    private final String uploadPath;
+
+    public FolderUtilImpl(@Value("${upload-path}") String uploadPath) {
+        this.uploadPath = uploadPath;
+    }
 
     public Path getUploadPath() throws IOException {
         return Paths.get(this.uploadPath.strip()).toRealPath(LinkOption.NOFOLLOW_LINKS);
