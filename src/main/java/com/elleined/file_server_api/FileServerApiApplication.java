418e2662b0da9f5bae867e32d2bbf194fbef2717
Elleined <demadegu@gmail.com>
2025-08-31 22:21:37 +0800

    Add discovery client dependency and enable discovery client

--- src/main/java/com/elleined/file_server_api/FileServerApiApplication.java
+++ src/main/java/com/elleined/file_server_api/FileServerApiApplication.java
@@ -2,8 +2,10 @@ package com.elleined.file_server_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FileServerApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(FileServerApiApplication.class, args);
